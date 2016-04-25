2087702f915d413ffe6e0abfa1d94b1beacb840c
aurelien <adam_a@etna-alternance.net>
2016-04-25 11:17:06 +0200

    base

---
+++ app/src/main/java/androiduniverse/pokeapi/MyActivity.java
@@ -0,0 +1,31 @@
+package androiduniverse.pokeapi;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mashape.unirest.http.Unirest;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        //ArticleFragment articleFragment = new ArticleFragment();
+    }
+
+
+
+    /*public class ArticleFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.activity_my, container, false);
+        }
+    }*/
+}
