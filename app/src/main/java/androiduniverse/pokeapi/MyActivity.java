7de49c1ed48ef65e73c96656b05e8dc092a05d31
lin_d <lin_d@etna-alternance.net>
2016-04-26 09:19:39 +0200

    unirest
    unirest > volley

--- app/src/main/java/androiduniverse/pokeapi/MyActivity.java
+++ app/src/main/java/androiduniverse/pokeapi/MyActivity.java
@@ -1,13 +1,13 @@
 package androiduniverse.pokeapi;
 
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.mashape.unirest.http.Unirest;
+import android.widget.TextView;
+
+
+
 
 public class MyActivity extends AppCompatActivity {
 
@@ -15,6 +15,24 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        TextView test = (TextView)findViewById(R.id.text);
+        //AsyncTask<String, String, String> text = new httpcall().execute("http://pokeapi.co/api/v2/pokemon/1");
+        test.setText("test");
+        //test.setText(httpcall.getinfo("http://pokeapi.co/api/v2/pokemon/1"));
+      /*  HttpResponse<JsonNode> response = null;
+
+        // These code snippets use an open-source library. http://unirest.io/java
+       try {
+             response = Unirest.get("https://phalt-pokeapi.p.mashape.com/pokemon/1/")
+                     .header("X-Mashape-Key", "zt7UYnPZsrmshGb9MJjLRCwh0Ly0p1q5O5Kjsn1dHVW2qAZugj")
+                     .asJson();
+           test.setText("ok");
+
+           Log.v("test", "ok");
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+*/
         //ArticleFragment articleFragment = new ArticleFragment();
     }
 
